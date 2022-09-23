9fef22523ff8e9c8b559ee8f98fafe9828ea3e29
Steven Dornan <72313187+stevendornan@users.noreply.github.com>
2022-09-23 10:46:47 +0100

    Module aliases

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import Layout from '../components/Layout';
+import Layout from '@/components/Layout';
 const AboutPage = () => {
   return (
 	<Layout title="About Coding Events"><h1>About</h1><p>This is an app to find the latest coding events </p><p>Version: 1.0.0</p></Layout>
